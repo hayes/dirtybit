6569a78b1d3ae07d5f170a47ce30a8b7cf384087
Michael Hayes <michael.hayes@urbanairship.com>
2014-07-28 21:48:53 -0700

    added dep graph and deregister method

--- test/lookup.test.js
+++ test/lookup.test.js
@@ -10,7 +10,7 @@ test('filters', function(t) {
 
   var instance = DirtyBit(val)
 
-  t.plan(3)
+  t.plan(6)
 
   instance.register('a.b - a.c', function(val) {
     t.strictEqual(val, 5)
@@ -23,4 +23,13 @@ test('filters', function(t) {
   instance.register('((a.c - a.b) + "").length', function(val) {
     t.strictEqual(val, 2)
   })
+
+  instance.register('a.e + a.f', dereg)
+  instance.deregister('a.e + a.f', dereg)
+
+  t.notOk(instance.events._events['a.e'])
+  t.notOk(instance.events._events['a.f'])
+  t.notOk(instance._events['a.e + a.f'])
+
+  function dereg() {}
 })
